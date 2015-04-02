69c5b3e8f873882a59f2341ca04cfec7b49e680b
wateer <liningning@wandoujia.com>
2015-04-02 18:03:18 +0800

    add aws & kernel_conf.py

---
+++ aws/script/post_install.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+
+region=
+
+
+hostname=
+hostname $hostname
+sed -i "s/^HOSTNAME=.*/HOSTNAME=$hostname/g" /etc/sysconfig/network
+
+
+if test -b /dev/sdb
+then
+    echo "resize2fs /dev/sdb ;sed -i '/resize2fs/d' /etc/rc.d/rc.local " >>/etc/rc.d/rc.local
+    mount -t ext4 /dev/sdb /home/
+    /sbin/blkid |egrep -v "vda" |sort -u -k1 |awk '{print $2" /home/ ext4    nosuid,noatime   1 2"}' >>/etc/fstab
+elif test -b /dev/xvdb
+then
+    echo "resize2fs /dev/xvdb ;sed -i '/resize2fs/d' /etc/rc.d/rc.local " >>/etc/rc.d/rc.local
+    mount -t ext4 /dev/xvdb /home/
+    /sbin/blkid |egrep -v "vda" |sort -u -k1 |awk '{print $2" /home/ ext4    nosuid,noatime   1 2"}' >>/etc/fstab
+fi
+
+
+mkdir -p /usr/local/bin/op/
+echo 'export PATH=$PATH:/usr/local/bin/op' >>/etc/profile
+
+
+dns_vip=
+sed -i "/nameserver/s/.*/nameserver ${dns_vip}/g" /etc/resolv.conf
+
+
+yum -y install bind
+ns_servers=
+
+private_key_path="Kamazonaws.com.cn.+157+30769.private"
+cat >${private_key_path} <<EOF
+Private-key-format: v1.3
+Algorithm: 157 (HMAC_MD5)
+Key: faFypFIday6z+LXB1+jQlMdH5kNPQuYHYo1HkNHjaFhT678x82R12OgyRB6sUta1xTXmEjdbHm0YiTTs1DKfLQ==
+Bits: AAA=
+Created: 20140827035505
+Publish: 20140827035505
+Activate: 20140827035505
+EOF
+chmod 400 ${private_key_path}
+public_key_path="Kamazonaws.com.cn.+157+30769.key"
+echo "amazonaws.com.cn. IN KEY 0 3 157 faFypFIday6z+LXB1+jQlMdH5kNPQuYHYo1HkNHjaFhT678x82R12Ogy RB6sUta1xTXmEjdbHm0YiTTs1DKfLQ==" >${public_key_path}
+
+local_domain=$(curl -w "\n" http://169.254.169.254/latest/meta-data/local-hostname |sed "s/^[^.]\+.//g")
+local_ipv4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+local_ipv4_reverse=$(echo ${local_ipv4} |awk -F"." '{print $4"."$3"."$2"."$1}')
+
+
+for ns_server in ${ns_servers}
+do
+	cat <<EOF | /usr/bin/nsupdate -k ${private_key_path} -v
+server ${ns_server}
+zone ${local_domain}
+update delete ${hostname}.${local_domain} A
+update add ${hostname}.${local_domain} 60 A ${local_ipv4}
+show
+send
+EOF
+
+	cat <<EOF | /usr/bin/nsupdate -k ${private_key_path} -v
+server ${ns_server}
+zone in-addr.arpa
+update delete ${local_ipv4_reverse}.in-addr.arpa PTR
+update add ${local_ipv4_reverse}.in-addr.arpa 60 PTR ${hostname}.${local_domain}
+show
+send
+EOF
+done
+
+
+/bin/rm -rf ${private_key_path}
+/bin/rm -rf ${public_key_path}
+if ! echo $hostname |grep dns 
+then
+	/bin/rpm -e bind
+fi
+
+/bin/rm -f /etc/yum.repos.d/*
+rpm --force -Uvh http://${region}.repo.nosa.com/nosa/6/x86_64/nosa-release-0.0.18-1.el6.x86_64.rpm
+yum -y install puppet
+/usr/bin/puppet agent --onetime --no-daemonize --server=${region}.puppetlb.nosa.com --ca_server=${region}.puppetca.nosa.com
+
+
+reboot
---
+++ scripts/kernel_conf.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import subprocess
+
+
+def shell(cmd):
+    """ 执行命令.
+
+    """
+    process = subprocess.Popen(
+        args=cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    std_out, std_err = process.communicate()
+    return_code = process.poll()
+    return return_code, std_out, std_err
+
+
+def kernel_conf(key, value, oper):
+    """ 根据条件修改内核参数.
+
+    key 是内核选项;
+    value 是内核选项的值;
+    oper 是操作方式.
+
+    如果 oper 是 equal 时, 当前系统 key 的值不等于 value 时, 则修改成 value
+    如果 oper 是 less 时, 当前系统 key 的值小于 value 时, 则修改成 value
+    如果 oper 是 great 时, 当前系统 key 的值大于 value 时, 则修改成 value
+
+    """
+    cmd = """sysctl %s |awk -F "=" '{print $NF}'""" % key
+    rc, so, se = shell(cmd)
+    curr_value = so.strip()
+    if oper == "equal":
+        if curr_value != str(value):
+            cmd = "sysctl -w %s=%s" % (key, value)
+            rc, so, se = shell(cmd)
+            return so
+    elif oper == "less":
+        if int(curr_value) < value:
+            cmd = "sysctl -w %s=%s" % (key, value)
+            rc, so, se = shell(cmd)
+            return so
+    elif oper == "great":
+        if int(curr_value) > value:
+            cmd = "sysctl -w %s=%s" % (key, value)
+            rc, so, se = shell(cmd)
+            return so
+
+
+def main():
+    equal_dict = {
+        "vm.swappiness": 0,
+        "net.ipv4.tcp_tw_reuse": 1,
+        "net.ipv4.tcp_tw_recycle": 0,
+        "net.ipv4.conf.default.rp_filter": 1,
+        "net.ipv4.tcp_syncookies": 1,
+        "kernel.sysrq": 0,
+        "kernel.msgmnb": 65536,
+        "kernel.msgmax": 65536,
+        "kernel.shmmax": 68719476736,
+        "kernel.shmall": 4294967296,
+        "kernel.panic": 60,
+        "net.ipv4.icmp_echo_ignore_broadcasts": 1,
+        "net.ipv4.icmp_ignore_bogus_error_responses": 1,
+        "net.ipv4.conf.all.accept_redirects": 0,
+        "net.ipv4.conf.all.rp_filter": 0,
+        "net.ipv4.conf.all.log_martians": 1,
+        "net.ipv4.conf.all.arp_announce": 2,
+        "net.ipv4.conf.all.arp_ignore": 1,
+        "net.ipv4.tcp_timestamps": 0,
+        "net.ipv4.tcp_synack_retries": 2,
+        "net.ipv4.tcp_syn_retries": 2,
+        "net.ipv4.tcp_fin_timeout": 30,
+        "net.ipv4.tcp_keepalive_time": 600,
+        "net.ipv4.tcp_keepalive_intvl": 15,
+        "net.ipv4.tcp_keepalive_probes": 5,   
+    }
+
+    less_dict = {
+        "net.ipv4.tcp_max_syn_backlog": 65536,
+        "net.core.netdev_max_backlog":  32768,
+        "net.core.somaxconn": 32768,
+        "net.ipv4.tcp_max_orphans": 3276800,
+        "net.ipv4.tcp_max_tw_buckets": 524288,
+        "fs.file-max": 1024000
+    }
+
+    for equal in equal_dict:
+        print kernel_conf(equal, equal_dict[equal], "equal")
+
+    for less in less_dict:
+        print kernel_conf(less, less_dict[less], "less")
+
+    # 对net.ipv4.tcp_mem 特殊处理一下.
+    cmd = """sysctl net.ipv4.tcp_mem |awk -F "=" '{print $NF}' |\
+            awk '{print $1" "$2" "$3}'""" 
+    rc, so, se = shell(cmd)
+    if so.strip() != "94500000 915000000 927000000":
+        cmd="""sysctl -w net.ipv4.tcp_mem='94500000 915000000 927000000'"""
+        rc, so, se = shell(cmd)
+        print so
+
+
+if __name__ == '__main__':
+    main()
